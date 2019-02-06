b0129234967c11b3a93060594006b1ecb19f8b86
Krzys Grochal <16109223+ExoticHandle@users.noreply.github.com>
2019-02-06 08:56:20 -0500

    Update Alexap2.py

--- Alexap2.py
+++ Alexap2.py
@@ -45,7 +45,7 @@ def Gpio_Intent(status,room):
     else:
         return statement('Sorry not possible.')
 #router
- @ask.intent('Router', mapping = {'status':'status'})
+@ask.intent('Router', mapping = {'status':'status'})
 def Gpio_Intent(status,room):
     if status in STATUSON:
 	GPIO.output(22,GPIO.HIGH)
